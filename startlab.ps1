671422648ab0303e5ea235d2d3857eafe9adc5e3
MIKEYBOYPC\edree <michael.endrizzi@gmail.com>
2023-06-19 13:16:27 +0200

    fixed startup

--- startlab.ps1
+++ startlab.ps1
@@ -1,4 +1,5 @@
 get-job| stop-job | remove-job 
+$pwd=get-location
 start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.go $name $port} -ArgumentList $pwd,"ParentFrame","8081"
 start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.go $name $port} -ArgumentList $pwd,"iframe1","3000"
 start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.go $name $port} -ArgumentList $pwd,"iframe2","3001"
