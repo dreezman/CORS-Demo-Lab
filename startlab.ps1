083dc288dbf7512cbc312beaa4a310495f32a7b3
MIKEYBOYPC\edree <michael.endrizzi@gmail.com>
2023-06-27 08:07:57 +0200

    fixed unix startup

--- startlab.ps1
+++ startlab.ps1
@@ -4,4 +4,4 @@ start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.g
 start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.go $name $port} -ArgumentList $pwd,"iframe1","3000"
 start-job -scriptblock {param($pwd,$name,$port) set-location $pwd; go run main.go $name $port} -ArgumentList $pwd,"iframe2","3001"
 write-host "`n"
-echo "In 20 seconds, use this URL: http://localhost:8081/?iframeurl1=http://localhost:3000/iframes.html&iframeurl2=http://localhost:3001/iframes.html"
+echo "In 20 seconds, use this URL: http://localhost:9081/?iframeurl1=http://localhost:3000/iframes.html&iframeurl2=http://localhost:3001/iframes.html"
