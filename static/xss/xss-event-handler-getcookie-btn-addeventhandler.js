b35ba2373ad8ec8bd426c4248739ad866a65321d
michael.endrizzi <michael.endrizzi@gmail.com>
2024-03-28 05:49:25 -0500

    still fixing. need to test at serviceware

---
+++ static/xss/xss-event-handler-getcookie-btn-addeventhandler.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myBtn").addEventListener("click", getCookie);
+});
+
--- static/xss/xss-getcookie.js
+++ static/xss/xss-getcookie.js
@@ -3,3 +3,5 @@ function getCookie(){
     document.cookie="TopSecretPassword=1234567890";
     alert('I see your cookies!!!:   ' + document.cookie)
 }
+
+
