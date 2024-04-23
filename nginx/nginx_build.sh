bf87c209ce2d50afbff2e2e9c5bc0106003b0741
michael Endrizzi <michael.endrizzi@gmail.com>
2024-04-23 17:12:33 +0200

    share with personal the gninx build

--- nginx/nginx_build.sh
+++ nginx/nginx_build.sh
@@ -1,6 +1,4 @@
 wd=`pwd`
-ngx_dir="${wd}/nginx"
-
 ngx_ver="1.25.5"
 ngx_tar_dir="nginx-${ngx_ver}"
 setnginx_ver="v0.33"
@@ -8,6 +6,10 @@ setnginx_tar_dir="set-misc-nginx-module-${setnginx_ver}"
 ngx_dev_kit_ver="v0.3.0"
 ngx_dev_kit_tar_dir="ngx_dev_kit-${ngx_dev_kit_ver}"
 
+cd ${wd}
+mkdir -pv nginx
+ngx_dir="${wd}/nginx"
+cd ${ngx_dir}
 sudo apt-get --yes --force-yes update && sudo apt-get upgrade
 sudo apt-get --yes --force-yes install gcc
 sudo apt-get --yes --force-yes make
@@ -21,13 +23,13 @@ sudo apt-get install  --yes --force-yes  libgd-dev
 sudo apt-get install  --yes --force-yes  net-tools
 
 
-cd ${ngx_dir}/
-wget -O  ${ngx_tar_dir} http://nginx.org/download/${ngx_tar_dir}.tar.gz
-tar -xzvf ${ngx_tar_dir}.tar.gz
+cd ${ngx_dir}/ ; mkdir -pv ${ngx_tar_dir} ${ngx_dev_kit_tar_dir} ${setnginx_tar_dir}
+wget -O  ${ngx_tar_dir}.tar.gz          http://nginx.org/download/${ngx_tar_dir}.tar.gz
+tar --strip-components 1 -C ${ngx_tar_dir}         -xzvf ${ngx_tar_dir}.tar.gz
 wget -O  ${ngx_dev_kit_tar_dir}.tar.gz  https://github.com/simpl/ngx_devel_kit/archive/${ngx_dev_kit_ver}.tar.gz
-tar -xzvf ${ngx_dev_kit_tar_dir}.tar.gz
-wget -O  ${setnginx_tar_dir}.tar.gz   https://github.com/openresty/set-misc-nginx-module/archive/${setnginx_ver}.tar.gz
-tar -xzvf ${setnginx_tar_dir}.tar.gz
+tar --strip-components 1 -C ${ngx_dev_kit_tar_dir} -xzvf ${ngx_dev_kit_tar_dir}.tar.gz
+wget -O  ${setnginx_tar_dir}.tar.gz     https://github.com/openresty/set-misc-nginx-module/archive/${setnginx_ver}.tar.gz
+tar --strip-components 1 -C ${setnginx_tar_dir}    -xzvf ${setnginx_tar_dir}.tar.gz
 
 cd ${ngx_dir}/${ngx_tar_dir}
  ./configure --prefix=/opt/nginx \
@@ -35,6 +37,6 @@ cd ${ngx_dir}/${ngx_tar_dir}
      --add-module=${ngx_dir}/${ngx_dev_kit_tar_dir}/ \
      --add-module=${ngx_dir}/${setnginx_tar_dir}/
 
- make -j2
- make install
+sudo  make -j2
+sudo  make install
 
