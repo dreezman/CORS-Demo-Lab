8646b563943a1d93faa5c7dd1a89dc2cb4aa22aa
michael Endrizzi <michael.endrizzi@gmail.com>
2024-04-24 15:36:33 +0200

    sharing

--- nginx/nginx_build.sh
+++ nginx/nginx_build.sh
@@ -1,31 +1,34 @@
 ##
 ## Build nginx from scratch and install it
 ##
-## $1 = Pass in root of repo, normally `pwd`
-##
-##
 
-repo_dir=$1
-ngx_dir="${repo_dir}/nginx/"
-ngx_ver="1.25.5"
-ngx_tar_dir="nginx-${ngx_ver}"
-setnginx_ver="v0.33"
-setnginx_tar_dir="set-misc-nginx-module-${setnginx_ver}"
-ngx_dev_kit_ver="v0.3.0"
-ngx_dev_kit_tar_dir="ngx_dev_kit-${ngx_dev_kit_ver}"
+# get the environment variables from the nginx_build_env.sh
+# to config for this environment
+#
+
+if [ -f "./nginx/nginx_build_env.sh" ]; then
+    echo "Found config file exists."
+    . ./nginx/nginx_build_env.sh 
+else
+    echo "Config file ./nginx/nginx_build_env.sh not found, must invoke from repo root. Exiting."
+    exit 1
+fi
+
+
+
 echo "******** Install pre-req modules **************************"
 cd ${ngx_dir}
-sudo apt-get --yes --force-yes update && sudo apt-get upgrade
-sudo apt-get --yes --force-yes install gcc
-sudo apt-get --yes --force-yes make
-sudo apt install --yes --force-yes make-guile
-sudo apt install --yes --force-yes libnginx-mod-rtmp
-sudo apt-get install  --yes --force-yes   zlib1g-dev zlib1g
-sudo apt-get install   --yes --force-yes libpcre3 libpcre3-dev
-sudo apt-get install   --yes --force-yes libssl-dev
-sudo apt-get install  --yes --force-yes  libperl-dev
-sudo apt-get install  --yes --force-yes  libgd-dev
-sudo apt-get install  --yes --force-yes  net-tools
+sudo apt-get --yes   update && sudo apt-get --yes upgrade
+sudo apt-get --yes   install gcc
+sudo apt-get --yes   make
+sudo apt install --yes   make-guile
+sudo apt install --yes   libnginx-mod-rtmp
+sudo apt-get install  --yes     zlib1g-dev zlib1g
+sudo apt-get install   --yes   libpcre3 libpcre3-dev
+sudo apt-get install   --yes   libssl-dev
+sudo apt-get install  --yes    libperl-dev
+sudo apt-get install  --yes    libgd-dev
+sudo apt-get install  --yes    net-tools
 
 echo "******** Install nginx nonce modules **************************"
 cd ${ngx_dir}/ ; mkdir -pv ${ngx_tar_dir} ${ngx_dev_kit_tar_dir} ${setnginx_tar_dir}
@@ -40,12 +43,38 @@ echo "******** configure nginx with nonces **************************"
 echo  ${ngx_dir} "---------->" ${ngx_tar_dir} " :together: " ${ngx_dir}/${ngx_tar_dir} ${ngx_dir}/${ngx_dev_kit_tar_dir} ${ngx_dir}/${setnginx_tar_dir}
 
 cd ${ngx_dir}/${ngx_tar_dir}
+# https://www.photographerstechsupport.com/tutorials/hosting-wordpress-on-aws-tutorial-part-2-setting-up-aws-for-wordpress-with-rds-nginx-hhvm-php-ssmtp/#nginx-source
  ./configure --prefix=/opt/nginx \
-     --with-http_ssl_module \
-     --add-module=${ngx_dir}/${ngx_dev_kit_tar_dir}/ \
-     --add-module=${ngx_dir}/${setnginx_tar_dir}/
+    --prefix=/etc/nginx \
+    --sbin-path=/usr/sbin/nginx \
+    --conf-path=/etc/nginx/nginx.conf \
+    --error-log-path=/var/log/nginx/error.log \
+    --http-log-path=/var/log/nginx/access.log \
+    --pid-path=/var/run/nginx.pid \
+    --lock-path=/var/run/nginx.lock \
+    --http-client-body-temp-path=/var/cache/nginx/client_temp \
+    --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+    --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+    --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+    --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+    --user=nginx \
+    --group=nginx \
+    --with-http_ssl_module \
+    --with-http_realip_module \
+    --with-http_gunzip_module \
+    --with-http_gzip_static_module \
+    --with-threads \
+    --with-file-aio \
+    --with-http_v2_module \
+    --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2-fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=native' \
+    # --add-module=../headers-more-nginx-module \
+    --with-http_ssl_module \
+    --add-module=${ngx_dir}/${ngx_dev_kit_tar_dir}/ \
+    --add-module=${ngx_dir}/${setnginx_tar_dir}/
+
 
 echo "******** Build nginx **************************"
 sudo  make -j2
 sudo  make install
-
+## use this to build docker image
+# https://stackoverflow.com/questions/28863126/creating-a-docker-image-with-nginx-compile-options-for-optional-http-modules
---
+++ nginx/nginx_build_env.sh
@@ -0,0 +1,13 @@
+##
+## Config file for nginx build environment
+##
+
+export repo_dir=`pwd` # assuming we are executing from the repo root
+export ngx_dir="${repo_dir}/nginx/"
+export ngx_ver="1.25.5"
+export ngx_tar_dir="nginx-${ngx_ver}"
+export setnginx_ver="v0.33"
+export setnginx_tar_dir="set-misc-nginx-module-${setnginx_ver}"
+export ngx_dev_kit_ver="v0.3.0"
+export ngx_dev_kit_tar_dir="ngx_dev_kit-${ngx_dev_kit_ver}"
+
